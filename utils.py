af3c6534f8a4d0dd110fdb728aaac0176875d388
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2022-06-13 20:34:14 +0300

    update user agent

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ bot = TelegramClient('utils_bot', config.API_ID, config.API_HASH).start(bot_toke
 
 def get_currency(ru=False, uah=False, crypto=False, other=False):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15'}
 
     list_currency = []
     if uah:
